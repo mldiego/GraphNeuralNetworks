dc3bb2b7e49e236eed9bf4f8ffc8dc21d9156bf8
Diego Manzanas <diego.manzanas.lopez@vanderbilt.edu>
2023-10-29 11:12:00 +0200

    Initial commit

---
+++ GNN_simple_reach.m
@@ -0,0 +1,60 @@
+%% Reachability analysis of a simple GNN
+% What do we have?
+
+% 1) In terms of Graph NN
+% Inputs (graph): X, A
+% Model (NN): Two GCN layers with relu activations
+
+% 2) In terms of NNV
+% ImageStar: can use this as a set representation
+% Poslin reach methods for Star and ImageStar
+% Matrix multiplications with ImageStars (affineMap?)
+
+% Time to start, make a prototype, and then we can try to generalize
+
+%% Load GNN and input (using GNN_simple_example.m)
+rng(77777);
+n_h = 4; %number of neurons in the hidden layer
+n_y = 2; %number of neurons in the output layer
+
+W0 = randn(size(X,2),n_h)*0.01;
+W1 = randn(n_h, n_y)*0.01;
+
+% Adjacency matrix (How the nodes are connected)
+A = [0 1 1 1 0 0;
+    1 0 1 0 0 0;
+    1 1 0 0 0 0;
+    1 0 0 0 1 1;
+    0 0 0 1 0 1;
+    0 0 0 1 1 0];
+% Node features matrix (values of node)
+X = [0 1 2 3 4 5]';
+
+%% Start the process
+
+A_ = A + eye(length(X)); % Self-loop
+D = diag(sum(A_,1)); %degree matrix
+D_inv_half = D^-(1/2);
+% DADX = D_inv_half*A*D_inv_half*X;
+IS = ImageStar(X,X); % Simplest test is when IB = UB
+
+% Transform input first outside of the layer methods
+DAD = D_inv_half*A*D_inv_half;
+
+L = ReluLayer(); % Create relu layer
+
+% Forward Propagation
+% Layer 1
+IS = IS.affineMap(DAD,[]);
+IS = IS.affineMap(W0,[]);
+R1 = L.reach(IS,'exact-star');
+% Layer 2
+Rt = R1.affineMap(DAD,[]);
+Rt = Rt.affineMap(W1,[]);
+R = L.reach(Rt,'exact-star');
+
+
+
+
+
+
---
+++ node_classification_example.m
@@ -0,0 +1,328 @@
+%% Training example using GCNs
+% https://www.mathworks.com/help/deeplearning/ug/node-classification-using-graph-convolutional-network.html
+
+%% Download data and preprocess it
+
+dataURL = "http://quantum-machine.org/data/qm7.mat";
+outputFolder = fullfile(tempdir,"qm7Data");
+dataFile = fullfile(outputFolder,"qm7.mat");
+
+if ~exist(dataFile,"file")
+    mkdir(outputFolder);
+    disp("Downloading QM7 data...");
+    websave(dataFile, dataURL);
+    disp("Done.")
+end
+
+data = load(dataFile);
+% Extract the Coulomb data and the atomic numbers from the loaded structure. 
+% Permute the Coulomb data so that the third dimension corresponds to the observations. 
+coulombData = double(permute(data.X, [2 3 1]));
+% Sort the atomic numbers in descending order.
+atomData = sort(data.Z,2,'descend');
+% convert data to adjacency form
+adjacencyData = coulomb2Adjacency(coulombData,atomData);
+
+% Visualize data
+figure
+tiledlayout("flow")
+
+for i = 1:9
+    % Extract unpadded adjacency matrix.
+    atomicNumbers = nonzeros(atomData(i,:));
+    numNodes = numel(atomicNumbers);
+    A = adjacencyData(1:numNodes,1:numNodes,i);
+
+    % Convert adjacency matrix to graph.
+    G = graph(A);
+
+    % Convert atomic numbers to symbols.
+    symbols = atomicSymbol(atomicNumbers);
+
+    % Plot graph.
+    nexttile
+    plot(G,NodeLabel=symbols,Layout="force")
+    title("Molecule " + i)
+end
+
+figure
+histogram(categorical(atomicSymbol(atomData)))
+xlabel("Node Label")
+ylabel("Frequency")
+title("Label Counts")
+
+% Partition data
+numObservations = size(adjacencyData,3);
+[idxTrain,idxValidation,idxTest] = trainingPartitions(numObservations,[0.8 0.1 0.1]);
+
+adjacencyDataTrain = adjacencyData(:,:,idxTrain);
+adjacencyDataValidation = adjacencyData(:,:,idxValidation);
+adjacencyDataTest = adjacencyData(:,:,idxTest);
+
+coulombDataTrain = coulombData(:,:,idxTrain);
+coulombDataValidation = coulombData(:,:,idxValidation);
+coulombDataTest = coulombData(:,:,idxTest);
+
+atomDataTrain = atomData(idxTrain,:);
+atomDataValidation = atomData(idxValidation,:);
+atomDataTest = atomData(idxTest,:);
+
+% convert data for training
+[AValidation,XValidation,labelsValidation] = preprocessData(adjacencyDataValidation,coulombDataValidation,atomDataValidation);
+
+% normalize training data
+muX = mean(XTrain);
+sigsqX = var(XTrain,1);
+
+XTrain = (XTrain - muX)./sqrt(sigsqX);
+XValidation = (XValidation - muX)./sqrt(sigsqX);
+
+
+%% Create neural network model
+
+% Initialize models
+parameters = struct;
+
+% Layer 1
+numHiddenFeatureMaps = 32;
+numInputFeatures = size(XTrain,2);
+
+sz = [numInputFeatures numHiddenFeatureMaps];
+numOut = numHiddenFeatureMaps;
+numIn = numInputFeatures;
+parameters.mult1.Weights = initializeGlorot(sz,numOut,numIn,"double");
+
+% Layer 2
+sz = [numHiddenFeatureMaps numHiddenFeatureMaps];
+numOut = numHiddenFeatureMaps;
+numIn = numHiddenFeatureMaps;
+parameters.mult2.Weights = initializeGlorot(sz,numOut,numIn,"double");
+
+% Layer 3
+classes = categories(labelsTrain);
+numClasses = numel(classes);
+
+sz = [numHiddenFeatureMaps numClasses];
+numOut = numClasses;
+numIn = numHiddenFeatureMaps;
+parameters.mult3.Weights = initializeGlorot(sz,numOut,numIn,"double");
+
+
+%% Training
+
+numEpochs = 1500;
+learnRate = 0.01;
+
+validationFrequency = 300;
+
+% initialize params for adam
+trailingAvg = [];
+trailingAvgSq = [];
+
+% convert data to dlarray for training
+XTrain = dlarray(XTrain);
+XValidation = dlarray(XValidation);
+
+% gpu?
+if canUseGPU
+    XTrain = gpuArray(XTrain);
+end
+
+% convert labels to onehot vector encoding
+TTrain = onehotencode(labelsTrain,2,ClassNames=classes);
+TValidation = onehotencode(labelsValidation,2,ClassNames=classes);
+
+% Visualize training progress
+monitor = trainingProgressMonitor( ...
+    Metrics=["TrainingLoss","ValidationLoss"], ...
+    Info="Epoch", ...
+    XLabel="Epoch");
+
+groupSubPlot(monitor,"Loss",["TrainingLoss","ValidationLoss"])
+
+
+epoch = 0; %initialize epoch
+
+% Begin training (custom train loop)
+while epoch < numEpochs && ~monitor.Stop
+    epoch = epoch + 1;
+
+    % Evaluate the model loss and gradients.
+    [loss,gradients] = dlfeval(@modelLoss,parameters,XTrain,ATrain,TTrain);
+
+    % Update the network parameters using the Adam optimizer.
+    [parameters,trailingAvg,trailingAvgSq] = adamupdate(parameters,gradients, ...
+        trailingAvg,trailingAvgSq,epoch,learnRate);
+
+    % Record the training loss and epoch.
+    recordMetrics(monitor,epoch,TrainingLoss=loss);
+    updateInfo(monitor,Epoch=(epoch+" of "+numEpochs));
+
+    % Display the validation metrics.
+    if epoch == 1 || mod(epoch,validationFrequency) == 0
+        YValidation = model(parameters,XValidation,AValidation);
+        lossValidation = crossentropy(YValidation,TValidation,DataFormat="BC");
+
+        % Record the validation loss.
+        recordMetrics(monitor,epoch,ValidationLoss=lossValidation);
+    end
+
+    monitor.Progress = 100*(epoch/numEpochs);
+end
+
+
+%% Testing
+
+[ATest,XTest,labelsTest] = preprocessData(adjacencyDataTest,coulombDataTest,atomDataTest);
+XTest = (XTest - muX)./sqrt(sigsqX);
+XTest = dlarray(XTest);
+
+YTest = model(parameters,XTest,ATest);
+YTest = onehotdecode(YTest,classes,2);
+
+accuracy = mean(YTest == labelsTest)
+
+% Visualize test results
+figure
+cm = confusionchart(labelsTest,YTest, ...
+    ColumnSummary="column-normalized", ...
+    RowSummary="row-normalized");
+title("GCN QM7 Confusion Chart");
+
+
+%% Predict using new data
+
+numObservationsNew = 4;
+adjacencyDataNew = adjacencyDataTest(:,:,1:numObservationsNew);
+coulombDataNew = coulombDataTest(:,:,1:numObservationsNew);
+
+predictions = modelPredictions(parameters,coulombDataNew,adjacencyDataNew,muX,sigsqX,classes);
+
+figure
+tiledlayout("flow")
+
+for i = 1:numObservationsNew
+    % Extract unpadded adjacency data.
+    numNodes = find(any(adjacencyDataTest(:,:,i)),1,"last");
+
+    A = adjacencyDataTest(1:numNodes,1:numNodes,i);
+
+    % Create and plot graph representation.
+    nexttile
+    G = graph(A);
+    plot(G,NodeLabel=string(predictions{i}),Layout="force")
+    title("Observation " + i + " Prediction")
+end
+
+
+
+
+%% Helper functions %%
+%%%%%%%%%%%%%%%%%%%%%%
+
+function [adjacency,features,labels] = preprocessData(adjacencyData,coulombData,atomData)
+
+    [adjacency, features] = preprocessPredictors(adjacencyData,coulombData);
+    labels = [];
+    
+    % Convert labels to categorical.
+    for i = 1:size(adjacencyData,3)
+        % Extract and append unpadded data.
+        T = nonzeros(atomData(i,:));
+        labels = [labels; T];
+    end
+    
+    labels2 = nonzeros(atomData);
+    assert(isequal(labels2,labels2))
+    
+    atomicNumbers = unique(labels);
+    atomNames =  atomicSymbol(atomicNumbers);
+    labels = categorical(labels, atomicNumbers, atomNames);
+
+end
+
+function [adjacency,features] = preprocessPredictors(adjacencyData,coulombData)
+
+    adjacency = sparse([]);
+    features = [];
+    
+    for i = 1:size(adjacencyData, 3)
+        % Extract unpadded data.
+        numNodes = find(any(adjacencyData(:,:,i)),1,"last");
+    
+        A = adjacencyData(1:numNodes,1:numNodes,i);
+        X = coulombData(1:numNodes,1:numNodes,i);
+    
+        % Extract feature vector from diagonal of Coulomb matrix.
+        X = diag(X);
+    
+        % Append extracted data.
+        adjacency = blkdiag(adjacency,A);
+        features = [features; X];
+    end
+
+end
+
+
+function Y = model(parameters,X,A)
+
+    ANorm = normalizeAdjacency(A);
+    
+    Z1 = X;
+    
+    Z2 = ANorm * Z1 * parameters.mult1.Weights;
+    Z2 = relu(Z2) + Z1;
+    
+    Z3 = ANorm * Z2 * parameters.mult2.Weights;
+    Z3 = relu(Z3) + Z2;
+    
+    Z4 = ANorm * Z3 * parameters.mult3.Weights;
+    Y = softmax(Z4,DataFormat="BC");
+
+end
+
+function [loss,gradients] = modelLoss(parameters,X,A,T)
+
+    Y = model(parameters,X,A);
+    loss = crossentropy(Y,T,DataFormat="BC");
+    gradients = dlgradient(loss, parameters);
+
+end
+
+function predictions = modelPredictions(parameters,coulombData,adjacencyData,mu,sigsq,classes)
+
+    predictions = {};
+    numObservations = size(coulombData,3);
+    
+    for i = 1:numObservations
+        % Extract unpadded data.
+        numNodes = find(any(adjacencyData(:,:,i)),1,"last");
+        A = adjacencyData(1:numNodes,1:numNodes,i);
+        X = coulombData(1:numNodes,1:numNodes,i);
+    
+        % Preprocess data.
+        [A,X] = preprocessPredictors(A,X);
+        X = (X - mu)./sqrt(sigsq);
+        X = dlarray(X);
+    
+        % Make predictions.
+        Y = model(parameters,X,A);
+        Y = onehotdecode(Y,classes,2);
+        predictions{end+1} = Y;
+    end
+
+end
+
+function ANorm = normalizeAdjacency(A)
+
+    % Add self connections to adjacency matrix.
+    A = A + speye(size(A));
+    
+    % Compute inverse square root of degree.
+    degree = sum(A, 2);
+    degreeInvSqrt = sparse(sqrt(1./degree));
+    
+    % Normalize adjacency matrix.
+    ANorm = diag(degreeInvSqrt) * A * diag(degreeInvSqrt);
+
+end
